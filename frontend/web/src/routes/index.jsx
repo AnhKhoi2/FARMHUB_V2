71d4fd91d639700a72d569f43f0896607ba36aca
quinnine <vqny0203@gmail.com>
2025-11-04 23:52:54 +0700

    chore(quinhni): wire ExpertHome route, fix imports and CSS (add temporary debug rules)

--- frontend/web/src/routes/index.jsx
+++ frontend/web/src/routes/index.jsx
@@ -1,13 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "../pages/Login";
 import Register from "../pages/register_v1.jsx";
 import Home from "../pages/Home";
+import ExpertHome from "../pages/ExpertHome";
 import PrivateRoute from "./PrivateRoute";
+import ExpertRoutes from "./ExpertRoutes";
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* root -> chuyển về login (luôn hiển thị login tại /) */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
 
         {/* Trang Login */}
         <Route path="/login" element={<Login />} />
@@ -15,15 +19,24 @@ export default function AppRoutes() {
         {/* Trang Register */}
         <Route path="/register" element={<Register />} />
 
-        {/* Trang cần đăng nhập */}
-        <Route 
-          path="/" 
+        {/* Trang cần đăng nhập (đổi sang /home) */}
+        <Route
+          path="/home"
           element={
             <PrivateRoute>
               <Home />
             </PrivateRoute>
-          } 
+          }
         />
+
+        {/* Expert area (nested routes under /expert) */}
+        <Route path="/expert/*" element={<ExpertRoutes />} />
+
+  {/* Direct expert home route for quick access/testing */}
+  <Route path="/experthome" element={<ExpertHome />} />
+
+        {/* Bắt mọi route khác về /login (tuỳ chọn) */}
+        <Route path="*" element={<Navigate to="/login" replace />} />
 
       </Routes>
     </BrowserRouter>
